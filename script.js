58398b9eed5957fed59c9010d5993f4c82c5baae
Andrea <90292543+amgallo1213@users.noreply.github.com>
2024-07-23 18:01:29 -0400

    adding a pop up

--- script.js
+++ script.js
@@ -1,18 +1,37 @@
-var modal = document.getElementById("myModal");
-var modalBtn = document.getElementById("myBtn");
+// var modal = document.getElementById("myModal");
+// var modalBtn = document.getElementById("myBtn");
 
-var span = document.getElementsByClassName("close")[0];
+// var span = document.getElementsByClassName("close")[0];
 
-modalBtn.onclick = function(){
-    modal.display = "block";
-}
+// modalBtn.onclick = function(){
+//     modal.display = "block";
+// }
 
-span.onclick = function(){
-    modal.display = "none";
-}
+// span.onclick = function(){
+//     modal.display = "none";
+// }
 
-window.onclick = function(event){
-    if (event.target == modal){
-        modal.display = "none";
+// window.onclick = function(event){
+//     if (event.target == modal){
+//         modal.display = "none";
+//     }
+// }
+
+
+function reveal() {
+    var reveals = document.querySelectorAll(".hidden");
+
+    for (var i = 0; i < reveals.length; i++) {
+        var windowHeight = window.innerHeight;
+        var elementTop = reveals[i].getBoundingClientRect().top;
+        var elementVisible = 150;
+
+        if (elementTop < windowHeight - elementVisible) {
+            reveals[i].classList.add("active");
+        } else {
+            reveals[i].classList.remove("active");
+        }
     }
 }
+
+window.addEventListener("scroll", reveal);
