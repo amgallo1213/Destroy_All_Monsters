9846362f29909fa0c48c289c3973195cab4c11c8
Andrea <90292543+amgallo1213@users.noreply.github.com>
2024-09-12 13:03:11 -0400

    added and styled popover

--- script.js
+++ script.js
@@ -1,37 +1,24 @@
-// var modal = document.getElementById("myModal");
-// var modalBtn = document.getElementById("myBtn");
+setTimeout(function(){
+    document.querySelector("#mypopover").showPopover()
+}, 3000)
 
-// var span = document.getElementsByClassName("close")[0];
 
-// modalBtn.onclick = function(){
-//     modal.display = "block";
-// }
 
-// span.onclick = function(){
-//     modal.display = "none";
-// }
 
-// window.onclick = function(event){
-//     if (event.target == modal){
-//         modal.display = "none";
+// function reveal() {
+//     var reveals = document.querySelectorAll(".hidden");
+
+//     for (var i = 0; i < reveals.length; i++) {
+//         var windowHeight = window.innerHeight;
+//         var elementTop = reveals[i].getBoundingClientRect().top;
+//         var elementVisible = 150;
+
+//         if (elementTop < windowHeight - elementVisible) {
+//             reveals[i].classList.add("active");
+//         } else {
+//             reveals[i].classList.remove("active");
+//         }
 //     }
 // }
 
-
-function reveal() {
-    var reveals = document.querySelectorAll(".hidden");
-
-    for (var i = 0; i < reveals.length; i++) {
-        var windowHeight = window.innerHeight;
-        var elementTop = reveals[i].getBoundingClientRect().top;
-        var elementVisible = 150;
-
-        if (elementTop < windowHeight - elementVisible) {
-            reveals[i].classList.add("active");
-        } else {
-            reveals[i].classList.remove("active");
-        }
-    }
-}
-
-window.addEventListener("scroll", reveal);
+// window.addEventListener("scroll", reveal);
